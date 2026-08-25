0d517e1da61fa98285ccb5b4c907e64b98ed27b2
mikikriki99 <mikikriki99@gmail.com>
2026-08-25 17:45:44 +0200

    Change account email and password in config.py
    Updated email and password for account configuration.

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
 
 # The email for your account (change it to your email)
-email = "example@domain.com"
+email = "mikikriki99@gmail.com"
 # The password for your account (change it to your password) 
-password = "examplepassword"
+password = "mikikriki99"
 # How many tokens to generate (change to specify how many tokens you want)
 amount = 1
